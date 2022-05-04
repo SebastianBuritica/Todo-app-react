10be8deff79cd2b989487f8679150a386f628853
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-05-04 13:10:05 -0500

    .

--- my-app/src/components/Formulario.jsx
+++ my-app/src/components/Formulario.jsx
@@ -1,20 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Formulario = () => {
+
+    const initialState = {
+        nombre: '',
+        descripcion: '',
+        estado: 'pendiente',
+        prioridad: 'false'
+    }
+
+    const [todo, setTodo] = useState(initialState)
+
+    const {nombre, descripcion, estado, prioridad} = todo
+
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
+    const handleChange = e => {
+        const {name, value, checked, type} = e.target
+
+        setTodo((old) => ({
+            ...old,
+            [name]: type === 'checkbox' ? checked : value
+        }))
+    }
+
   return (
     <>
         <h3>Agregar TODO</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input 
             type="text" 
             className='form-control mb-2'
             name='nombre'
             placeholder='ingrese todo nombre'
+            value={nombre}
+            onChange={handleChange}
             />
-            <textarea 
-            placeholder='form-control mb-2'
+            
+            <textarea
+            className='form-control mb-2' 
+            placeholder='ingrese descripcion'
             name='descripcion'
+            value={descripcion}
+            onChange={handleChange}
             />
+
+            <select 
+            name="estado" 
+            className='form-control mb-2' 
+            value={estado}
+            onChange={handleChange}
+            >
+                <option value="pendiente">pendiente</option>
+                <option value="completado">completado</option>
+            </select>
+
+            <div className="form-check">
+            <input 
+            className="form-check-input" 
+            type="checkbox" 
+            id="flexCheckDefault" 
+            name='prioridad'
+            checked={prioridad}
+            onChange={handleChange}
+            />
+            
+            <label 
+            className="form-check-label" 
+            htmlFor="flexCheckDefault">
+                Default checkbox
+            </label>
+            </div>
+
+            <button type='submit' className='btn btn-primary'>
+                Agregar
+            </button>
+
         </form>
     </>
   )
